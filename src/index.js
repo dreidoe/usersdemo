8a08ace1733fa0cfc9535c6d0e8c3896dff41e3c
Aundrei Bryant <drei97@gmail.com>
2022-12-14 12:21:05 -0600

    fetch users

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 const root = document.querySelector("#root");
-
+root.innerHTML = "<h2 class= ' text-violet-500'>Hello World</h2>";
 console.log(root);
+
+const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+const users = await resp.json();
+
+console.log(users);
