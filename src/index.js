56e4b3a631f056b4532b2774c6a40449d3c2d5d5
Aundrei Bryant <drei97@gmail.com>
2022-12-14 14:06:31 -0600

    Great example of how to push to browser with function and map

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const root = document.querySelector("#root");
-root.innerHTML = "<h2 class= ' text-violet-500'>Hello World</h2>";
+
 console.log(root);
 
 const resp = await fetch("https://jsonplaceholder.typicode.com/users");
