f8e0126942a859ea83950ec9894c1834098a6f6c
Aundrei Bryant <drei97@gmail.com>
2022-12-15 10:37:29 -0600

    bioCards put in a grid

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ console.log(users);
 
 function createBioCard(user) {
   return `
-    <section class="bg-slate-900 text-white p-6 w-3/12 min-w-max rounded-md">
+    <section class="bg-slate-900 text-white p-6  min-w-min rounded-md">
     <h2 class="text-2xl font-semibold my-2">${user.name}</h2>
-    <ul class="flex gap-x-4 my-4">
+    <ul class="flex flex-col gap-y-4 my-4">
       <li><a href="mailto:${user.email}">${user.email}</a></li>
       <li><a href="tel:${user.phone}">${user.phone}</a></li>
       <li><a href="https://www.google.com/maps/place/${user.address.geo.lat},${user.address.geo.lng}">${user.address.street}, ${user.address.city}</a></li>
@@ -21,6 +21,14 @@ function createBioCard(user) {
   </section>
     `;
 }
+root.classList.add(
+  "container",
+  "mx-auto",
+  "items-center",
+  "grid",
+  "grid-cols-3",
+  "gap-4"
+);
 
 const bioCardsHTML = users.map(createBioCard).join("");
 
